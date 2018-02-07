a403fa049344876abc0151ab6d01e74b5c444072
Boris Berdichevskiy <bberdich@gmail.com>
2018-02-07 18:18:24 -0500

    added 1.5

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
 
 // console.log(perm('abcd'));
 
-//1.5
+//1.3
 function removingSpaces(string) {
   const stringArr = string.split('');
   for (let i = 0; i < string.length; i++) {
@@ -36,4 +36,34 @@ function removingSpaces(string) {
   }
   if (stringArr[stringArr.length - 1] === '%20') stringArr.pop();
   return stringArr.join('');
+}
+
+
+//1.5
+function oneAway(str1, str2) {
+  let didMod = false;
+  const len1 = str1.length;
+  const len2 = str2.length;
+  let longer = len1 > len2 ? str1 : str2;
+  const shorter = len1 < len2 ? str1 : str2;
+  if (len1 === len2) {
+    for (let i = 0; i < len1; i++) {
+      console.log(str1[i], str2[i], didMod)
+      if (str1[i] !== str2[i]) {
+        if (didMod === true) return false;
+        didMod = true;
+      }
+    }
+  } else {
+    for (let i = 0; i < longer.length; i++) {
+      console.log(i, longer[i], shorter[i], didMod)
+      if (longer[i] !== shorter[i]) {
+        if (didMod === true) return false;
+        longer = longer.slice(0, i-1) + longer.slice(i)
+        didMod = true;
+        i--;
+      }
+    }
+  }
+  return true;
 }
